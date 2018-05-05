e62fabe5ea5bd226935efaf1d964371e44712591
Topherian <topherianlimited@foo.com>
2018-05-05 23:39:13 +0100

    update script to support subdirs

--- test.sh
+++ test.sh
@@ -1,23 +1,35 @@
 #!/bin/bash     
-# 2018-03-03 - Scans manifest directory and removes spaces 
+# 2018-03-03 - Scans manifest directory and removes spaces from files
      
-MANIFEST_DIR=Data
-MANIFEST_EXT=.txt
 RUN_DATE=$(date +%Y-%m-%d)
-LOGFILE=/Users/christopherbautista/src/sandbox-bash/test-"$RUN_DATE".log
+LOGFILE=./manifest-space-remover-"$RUN_DATE".log
 exec 3>&1 4>&2
 exec 1>>"$LOGFILE" 2>&1
 
-echo "$(date "+%Y-%m-%d %T") : Starting rename check in directory: $MANIFEST_DIR " 
-for file in "$MANIFEST_DIR"/*"$MANIFEST_EXT"; do
-    [ -e "$file" ] || continue
-    echo "$(date "+%Y-%m-%d %T") : Checking file - $file" 
+function traverse_manifests() {
+    for file in "$1"/*
+    do
+        if [ ! -d "${file}" ] ; then
+            [ -e "$file" ] || continue 
+            echo "$(date "+%Y-%m-%d %T") : Checking file - $file" 
+            #check if file has a space, if so rename
+            if [[ "$file" =~ \ |\' ]]
+            then
+                echo "$(date "+%Y-%m-%d %T") : $file contains a space, so will remove and rename."
+                mv "$file" "${file// /}"
+                echo "$(date "+%Y-%m-%d %T") : $file has been renamed with the space removed."
+            fi
+        else
+            echo "$(date "+%Y-%m-%d %T") : Traversing to: ${file}"
+            traverse_manifests "${file}"
+        fi
+    done
+}
 
-    #check if file has a space, if so rename
-    if [[ "$file" =~ \ |\' ]] 
-    then
-        echo "$(date "+%Y-%m-%d %T") : $file contains a space, so will rename." 
-        mv "$file" "${file// /}"
-    fi
-done
+function main() {
+    echo "$(date "+%Y-%m-%d %T") : Starting rename check in directory: $1" 
+    traverse_manifests "$1"
+}
+
+main "$1"
 echo "$(date "+%Y-%m-%d %T") : Rename process complete."
